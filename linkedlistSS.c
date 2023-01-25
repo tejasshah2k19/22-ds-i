53329c1b3dd6f10a759d6a9370e785e75cf30f8c
tejas <tejasshah@gmail.com>
2023-01-25 22:32:02 +0530

    ll

--- linkedlistSS.c
+++ linkedlistSS.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 struct node
 {
     int data;
@@ -9,14 +9,18 @@ struct node
 
 void addNode(int);
 void display();
-
+void delLast();
+void delBeg();
+void search();
+void addBeg();
+void deleteAny();//ask num and delete 
 int main()
 {
     int choice, num;
 
     while (1)
     {
-        printf("\n0 For Exit\n1 For Add Node\n2 For Display");
+        printf("\n0 For Exit\n1 For Add Node\n2 For Display\n3 For DeleteLast\n4 For Del Beg\n5 For Search\n6 For add Beg");
         printf("\nEnter your choice");
         scanf("%d", &choice);
 
@@ -31,6 +35,15 @@ int main()
             break;
         case 2:
             display();
+            break;
+        case 3:
+            delLast();
+            break;
+        case 4:
+            delBeg();
+            break;
+        case 5:
+            search();
             break;
         default:
             printf("\nPTA");
@@ -79,4 +92,65 @@ void display()
           // p->next == NULL
           // p!=NULL
     }
+}
+
+void delLast()
+{
+
+    struct node *p = head;
+
+    while (p->next != last)
+    {
+        p = p->next;
+    }
+
+    p->next = NULL;
+    free(last);
+    last = p;
+}
+
+void delBeg()
+{
+    struct node *p = head;
+    head = head->next;
+    free(p);
+}
+
+void search()
+{
+    int s;
+    struct node *p;
+    bool isFound = 0; // c99
+    printf("\nEnter search num");
+    scanf("%d", &s);
+
+    p = head;
+
+    while (p != NULL)
+    {
+        if (p->data == s)
+        {
+            isFound = 1;
+            break;
+        }
+        p = p->next;
+    }
+
+    isFound == 1 ? printf("\n%d Found", s) : printf("\n%d not Found", s);
+}
+
+void addBeg(){
+    int num;
+    struct node *tmp; 
+    printf("\nEnter number");
+    scanf("%d",&num);
+
+    tmp = malloc(sizeof(struct node));
+    tmp->data = num;
+    tmp->next = head; 
+    head = tmp ;
+}
+
+void deleteAny(){
+    
 }
